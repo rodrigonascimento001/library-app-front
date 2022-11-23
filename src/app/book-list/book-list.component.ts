7abf61b33ad023fc562e1492deb3ff322b55ed14
Rodrigo Alexandre <rodrigo.bootcampclouduser@hotmail.com>
2022-11-23 17:53:33 -0300

    configuracao inicial do projeto

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'; 
+import { Book } from '../book';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'] 
+})
+export class BookListComponent  implements OnInit {
+
+  books: Book[];
+
+  constructor(private bookService: BookService,
+    private router: Router) { 
+      this.books = [];
+    }
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  private getBooks(){
+    this.bookService.getBooksList().subscribe(data => {
+      this.books = data;
+    });
+  }
+
+  bookDetails(id: number){
+    this.router.navigate(['book-details', id]);
+  }
+
+  updateBook(id: number){
+    this.router.navigate(['book-employee', id]);
+  }
+
+  deleteBook(id: number){
+    this.bookService.deleteBook(id).subscribe( data => {
+      console.log(data);
+      this.getBooks();
+    })
+  }
+
+
+
+}
---
+++ src/app/book.ts
@@ -0,0 +1,8 @@
+export class Book {
+    id!: number;
+    name!: string;
+    price!: number;
+    pages! : number;
+    description!: string;
+    pathImage!: string;
+}
